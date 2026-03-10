0e7d274a287c1ed440780c165592230e6a9bc69b
Periyanan <itzmeperi@gmail.com>
2026-03-11 00:21:40 +0530

    UI

--- _pages_legacy/Federation.tsx
+++ _pages_legacy/Federation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import { subscribeToDocument } from '../lib/adminDb';
 import LiveEditable from '../components/admin/LiveEditable';
--- _pages_legacy/Shop.tsx
+++ _pages_legacy/Shop.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from 'react';
 import ProductCard from '../components/ProductCard';
 import { subscribeToCollection } from '../lib/adminDb';
